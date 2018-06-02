85a790786ca1dcf8adf3e8970ededf144156bea0
Gokul <gokulps15@gmail.com>
2018-06-02 23:27:13 +0530

    added exceptions

---
+++ toasts/exceptions.py
@@ -0,0 +1,27 @@
+
+"""
+toasts.exceptions
+~~~~~~~~~~~~~~~~~
+
+Contains exceptions raised by toasts
+"""
+
+
+class ToastError(Exception):
+    """Base class for all toasts exceptions."""
+    pass
+
+
+class AuthError(ToastError):
+    """Raised when invalid credentials are supplied"""
+    def __init__(self, client):
+        msg = 'Username or password is invalid for {}'.format(client)
+        super().__init__(msg)
+
+
+class UnexpectedResponse(ToastError):
+    """Raised when the status code of an operation is not the expected one"""
+    def __init__(self, client, status_code):
+        msg = 'Unexpected response from {0} (status code: {1})'.format(
+                client, status_code)
+        super().__init__(msg)
