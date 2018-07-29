43aa0652f1662f4c4ea9ca59c0e5cb710a1c2aaf
Gokul <gokulps15@gmail.com>
2018-07-29 22:51:10 +0530

    added wrappers for showing notifications

--- toasts/wrappers.py
+++ toasts/wrappers.py
@@ -3,10 +3,29 @@
 toasts.wrappers.py
 ~~~~~~~~~~~~~~~~~~
 
-Wrapper classes for the app.
+Wrapper objects for the app.
 """
 
+import plyer
 import configobj
+
+from . import util
+
+
+def show_notif(title, msg, icon):
+    """Show a notification with `title` and description `msg`"""
+    icon_path = util.get_icon(icon)
+    plyer.notification.notify(
+        title=title,
+        message=msg,
+        app_icon=icon_path,
+        app_name='toasts',
+        timeout=7
+    )
+
+def show_error(msg, title='An error occured in Toasts'):
+    """Show an error message as notification"""
+    show_notif(title=title, msg=msg, icon='error')
 
 
 class Preferences:
