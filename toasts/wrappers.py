b63a3d7cd3b59cc39dded8d9d0210e068d119bfa
Gokul <gokulps15@gmail.com>
2018-08-27 21:20:01 +0530

    add Notifier object for showing notifications

--- toasts/wrappers.py
+++ toasts/wrappers.py
@@ -15,20 +15,37 @@ import requests
 from . import util
 
 
-def show_notif(title, msg, icon):
-    """Show a notification with `title` and description `msg`"""
-    icon_path = util.get_icon(icon)
-    plyer.notification.notify(
-        title=title,
-        message=msg,
-        app_icon=icon_path,
-        app_name='toasts',
-        timeout=7
-    )
+class Notifier():
+    """
+    Show desktop notifications.
+    Attributes:
+        disp_timeout (int): Show a notification for this much seconds.
+    """
+    def __init__(self, timeout):
+        self.disp_timeout = timeout
 
-def show_error(msg, title='An error occured in Toasts'):
-    """Show an error message as notification"""
-    show_notif(title=title, msg=msg, icon='error')
+    def show_notif(self, title, msg, icon):
+        """
+        Show a notification.
+        Args:
+            title (str): Title of notification.
+            msg (str): Content of the notification.
+            icon (str): Name of icon to be used.
+                `icon` should be the name of a file in `toasts/data/icons/`,
+                stripped off it's extension. Eg: github (not github.png)
+        """
+        icon_path = util.get_icon(icon)
+        plyer.notification.notify(
+            title=title,
+            message=msg,
+            app_icon=icon_path,
+            app_name='toasts',
+            timeout=self.disp_timeout
+        )
+
+    def show_error(self, msg, title='An error occured in Toasts'):
+        """Show an error message as notification"""
+        self.show_notif(title=title, msg=msg, icon='error')
 
 
 class Session(requests.Session):
