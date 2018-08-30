9e72a3d1e6cbb8555391cd427cbccf330175ccc9
Gokul <gokulps15@gmail.com>
2018-08-30 17:22:05 +0530

    map client name to object

---
+++ toasts/clients/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+toasts.clients
+~~~~~~~~~~~~~~
+
+Contains client classes used for fetching notifications from various websites.
+"""
+
+from .github import GitHubClient
+
+CLIENTS = {
+    'github': GitHubClient,
+}
